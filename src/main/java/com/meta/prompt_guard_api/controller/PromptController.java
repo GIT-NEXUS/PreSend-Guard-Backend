be35b51e313fd59111b56bd7e5785a895adc1ebe
Jinny926 <qsdfg0926@gmail.com>
2026-02-21 21:26:43 +0900

    feat: 프롬프트 분석 API 컨트롤러 추가

--- src/main/java/com/meta/prompt_guard_api/controller/PromptController.java
+++ src/main/java/com/meta/prompt_guard_api/controller/PromptController.java
@@ -1,4 +1,25 @@
 package com.meta.prompt_guard_api.controller;
 
+import com.meta.prompt_guard_api.dto.PromptRequestDto;
+import com.meta.prompt_guard_api.dto.PromptResponseDto;
+import com.meta.prompt_guard_api.service.PromptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class PromptController {
+
+    private final PromptService promptService;
+
+    @PostMapping("/analyze")
+    public ResponseEntity<PromptResponseDto> analyze(@RequestBody PromptRequestDto dto){
+        PromptResponseDto response = promptService.analyze(dto);
+        return ResponseEntity.ok(response);
+    }
 }
