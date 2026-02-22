da7281ab0a2b2ee0ce9756f605f8ad26d13349b1
Jinny926 <qsdfg0926@gmail.com>
2026-02-23 01:34:19 +0900

    feat : API 요청 본문 매핑을 위한 PromptRequestDto 추가

--- src/main/java/com/meta/prompt_guard_api/dto/PromptRequestDto.java
+++ src/main/java/com/meta/prompt_guard_api/dto/PromptRequestDto.java
@@ -1,4 +1,11 @@
 package com.meta.prompt_guard_api.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PromptRequestDto {
+
+    private String text;
 }
