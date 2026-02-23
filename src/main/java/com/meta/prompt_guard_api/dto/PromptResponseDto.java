8d2ebf74626ea22fa26f003e65aedefb95dc50d7
Jinny926 <qsdfg0926@gmail.com>
2026-02-23 23:54:25 +0900

    feat : analyze API 응답용PromptRequestDto 추가

--- src/main/java/com/meta/prompt_guard_api/dto/PromptResponseDto.java
+++ src/main/java/com/meta/prompt_guard_api/dto/PromptResponseDto.java
@@ -1,4 +1,31 @@
 package com.meta.prompt_guard_api.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PromptResponseDto {
+
+    private final int score;
+    private final String action;
+    private final String maskedText;
+
+    private final int phoneCount;
+    private final int emailCount;
+    private final int rrnCount;
+
+    public PromptResponseDto(
+            int score,
+            String action,
+            String maskedText,
+            int phoneCount,
+            int emailCount,
+            int rrnCount
+    ){
+        this.score=score;
+        this.action=action;
+        this.maskedText=maskedText;
+        this.phoneCount=phoneCount;
+        this.emailCount=emailCount;
+        this.rrnCount=rrnCount;
+    }
 }
