a52abf1fd30055e81e253fb0e1c99cad42c0abee
Jinny926 <qsdfg0926@gmail.com>
2026-03-21 19:48:28 +0900

    feat: NER 탐지 결과 엔티티 추가

--- src/main/java/com/meta/prompt_guard_api/ner/NerEntityDto.java
+++ src/main/java/com/meta/prompt_guard_api/ner/NerEntityDto.java
@@ -1,4 +1,14 @@
 package com.meta.prompt_guard_api.ner;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NerEntityDto {
+    private String text;
+    private String label;
+    private double score;
+    private int start;
+    private int end;
 }
